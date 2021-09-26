646e80f6f5501d619cacc539d21d9e62376e70cd
Hulxv <m7mdx37@gmail.com>
2021-09-26 05:11:31 +0200

    Fix overflow

--- components/RightBar/index.js
+++ components/RightBar/index.js
@@ -6,7 +6,7 @@ export default function RightBar() {
 	return(
 		<div className={'w-full md:w-52 '}>
 
-			<div className={'md:fixed md:right-2 pt-2 md:p-8 md:pt-6 rounded-xl space-y-3 md:space-x-3 bg-white flex md:items-cente md:h-126 md:overflow-hidden md:overflow-y-auto flex-col'}>
+			<div className={'md:fixed md:right-2 pt-2 md:p-8 md:pt-6 rounded-xl space-y-3 md:space-x-3 bg-white flex md:items-cente md:h-126 overflow-hidden md:overflow-y-auto flex-col'}>
 
 				<div className={'p-4 md:p-0'}>
 					<NewPost />
