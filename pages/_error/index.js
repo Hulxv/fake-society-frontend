1aa424524d8ed719d93b453a1211d8dc1fb5c6ac
Hulxv <m7mdx37@gmail.com>
2021-10-01 03:52:14 +0200

    Add NavLinks, Settings, and more

--- pages/_error/index.js
+++ pages/_error/index.js
@@ -14,7 +14,7 @@ export default function NotFound() {
 			<title>Page Not Found</title>
 		</Head>
 
-		<div className={'w-full h-134 flex flex-col items-center justify-center space-y-8'}>
+		<div className={'w-full h-screen flex flex-col items-center justify-center space-y-8'}>
 			<div className={'w-full divide-black divide-x space-x-3 flex items-center justify-center'}>
 				<h1 className="text-2xl text-red-600">404</h1>
 				<div className={'p-2'}>Page could not found</div>
