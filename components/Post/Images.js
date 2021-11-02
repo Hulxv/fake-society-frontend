fabea03032644fb4599b4c76e89ec1ec24157d09
Hulxv <m7mdx37@gmail.com>
2021-11-02 02:32:38 +0200

    fix 'Warning: Do not use <img>. Use Image from 'next/image' instead.' #2

--- components/Post/Images.js
+++ components/Post/Images.js
@@ -1,5 +1,5 @@
-import Zoom from 'react-medium-image-zoom'
-import { Fragment } from 'react'
+import Zoom from "react-medium-image-zoom";
+import { Fragment } from "react";
 import {
 	Modal,
 	ModalOverlay,
@@ -8,69 +8,78 @@ import {
 	ModalFooter,
 	ModalBody,
 	ModalCloseButton,
-	useDisclosure, 
+	useDisclosure,
 	Button,
 	Avatar,
 	Divider,
-	Img
-  	
-} from '@chakra-ui/react'
+	Img,
+} from "@chakra-ui/react";
 
+export default function Images(props) {
+	const { ImagesList } = props;
+	const { isOpen, onOpen, onClose } = useDisclosure();
 
- export default function Images(props) {
- 	const {ImagesList} = props
- 	const { isOpen, onOpen, onClose } = useDisclosure()
+	return (
+		ImagesList !== null &&
+		ImagesList.length > 0 && (
+			<div className=' flex flex-col items-center space-y-2 w-full'>
+				<div className={"w-full justify-center flex flex-wrap"}>
+					{ImagesList.map(
+						(img, index) =>
+							index <= 2 && (
+								<Zoom key={index}>
+									<Img
+										src={img}
+										alt=''
+										className={"p-1 rounded-lg h-36 sm:h-60  overflow-hidden"}
+									/>
+								</Zoom>
+							),
+					)}
+				</div>
 
+				{ImagesList.length > 3 && (
+					<>
+						<Button onClick={onOpen} size='xs' variant='ghost'>
+							View All Images
+						</Button>
 
- 	return (ImagesList !== null && ImagesList.length > 0 ) && (
-	<div className=" flex flex-col items-center space-y-2 w-full">
-		<div className={'w-full justify-center flex flex-wrap'}>	
+						<Modal
+							isOpen={isOpen}
+							onClose={onClose}
+							scrollBehavior={"inside"}
+							size={"lg"}>
+							<ModalOverlay />
+							<ModalContent>
+								<ModalHeader>Notifications</ModalHeader>
+								<ModalCloseButton />
+								<ModalBody Spacing={2}>
+									<div className={"space-y-2 flex flex-col items-center"}>
+										{ImagesList.map((Image, index) => (
+											<Fragment key={index}>
+												<Zoom>
+													<Img
+														src={Image}
+														alt=''
+														className={"p-1 rounded-lg w-full overflow-hidden"}
+													/>
+												</Zoom>
+												<Divider />
+											</Fragment>
+										))}
+									</div>
+								</ModalBody>
 
-			{
-				ImagesList.map((img, index) => index <= 2 && (
-
-					<Zoom key={index}>
-						<Img src={img} alt="" className={'p-1 rounded-lg h-36 sm:h-60  overflow-hidden'}/>
-					</Zoom>
-
-				))
-			}
-		</div>
-	
-	
-		{ImagesList.length > 3 && <>
-		<Button onClick={onOpen} size='xs' variant="ghost">View All Images</Button>
-
-	      <Modal isOpen={isOpen} onClose={onClose}  scrollBehavior={'inside'} size={'lg'}>
-	        <ModalOverlay />
-	        <ModalContent>
-	          <ModalHeader>Notifications</ModalHeader>
-	          <ModalCloseButton />
-	          <ModalBody Spacing={2}>
-	            <div className={'space-y-2 flex flex-col items-center'}>
-	              {
-	              	ImagesList.map((Image, index) => (
-	                    <Fragment key={index}>
-	              			<Zoom>
-	              				<img src={Image} alt="" className={'p-1 rounded-lg w-full overflow-hidden'}/>
-
-	              			</Zoom>
-	                    	<Divider />
-	                    </Fragment>
-	              		))
-	              }
-	            </div>
-	          </ModalBody>
-
-	          <ModalFooter>
-	            <Button colorScheme="blue" mr={3} onClick={onClose}>
-	              Close
-	            </Button>
-	          </ModalFooter>
-	        </ModalContent>
-	      </Modal>
-		</>}
-
-	</div>
- 	)
- }
+								<ModalFooter>
+									<Button colorScheme='blue' mr={3} onClick={onClose}>
+										Close
+									</Button>
+								</ModalFooter>
+							</ModalContent>
+						</Modal>
+					</>
+				)}
+			</div>
+		)
+	);
+}
